698c6f998c8eb35f9ca7c29aa044f3af5e09ac40
Rachele Suy <rachelesuy@gmail.com>
2019-04-09 19:15:26 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -2,7 +2,7 @@
  
 board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
 def position_taken?(board, index)
-  if (board[index] == " " || board[index] == "" || board[index] == "")
+  if (board[index] == " " || board[index] == "" || board[index] == nil)
     return FALSE
   #elsif board[index] == ""
  #   return FALSE
