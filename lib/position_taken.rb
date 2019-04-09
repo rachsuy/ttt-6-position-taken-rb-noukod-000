5267c1b6562b849bf2b76111722d598a5e3d27de
Rachele Suy <rachelesuy@gmail.com>
2019-04-09 19:17:01 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -4,15 +4,10 @@ board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
 def position_taken?(board, index)
   if (board[index] == " " || board[index] == "" || board[index] == nil)
     return FALSE
-  #elsif board[index] == ""
- #   return FALSE
-  #  end
-  #  elsif board[index] == nil
-  #  return FALSE
- #   end
+
   else (board[index] == "X" ||  board[index] == "O")
-  #  return TRUE
-  # end
+   return TRUE
+  
   end
 
 end
